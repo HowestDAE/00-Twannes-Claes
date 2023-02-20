8a4e04cb65d0679501f54bc60cf7e8f6051c8ad5
Twannes <Twannes02@gmail.com>
2023-02-20 11:32:32 +0100

    4 exta excersises

---
+++ Console_Tests/Model/Hero.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Tests.Model
+{
+    public class Hero
+    {
+		private string _myNemesis;
+
+		public string MyNemesis { get; set; } = "Default name";
+
+
+		public string MyName { get; set; } = "Default name";
+
+		public int Level { get; set; }
+
+		public Hero(string name) : this(name, "Nemesis", 1)
+		{
+			this.MyName= name;
+		}
+
+		public Hero(string name, string myNemesis, int Level)
+		{
+			this.MyName= name;
+			this._myNemesis= myNemesis;
+			this.Level = Level;
+		}
+
+	}
+}
---
+++ Console_Tests/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 01
+            //Console.WriteLine("Hello world");
+            //Console.WriteLine("What's your name");
+            //string input = Console.ReadLine();
+            //
+            //Console.WriteLine($"Hello {input}");
+            //Console.ReadLine();
+            #endregion
+
+            #region 02
+            //Console.WriteLine("Enter a sequence of number seperated by space");
+            //
+            //string[] array = Console.ReadLine().Split(' ');
+            //
+            //double sum = 0, product = 1;
+            //
+            //foreach(string s in array)
+            //{
+            //    double number = Convert.ToDouble(s);
+            //    sum += number;
+            //    product *= number;
+            //}
+            //
+            //Console.WriteLine($"Sum: {sum} ");          
+            //Console.WriteLine($"Product: {product} ");      
+            //Console.ReadLine();
+            #endregion
+
+            #region 03
+
+            //Console.WriteLine("Enter a sentence");
+            //
+            //String  sentence = Console.ReadLine();
+            //
+            //string[] array = { };
+            //
+            //string word = string.Empty;
+            //
+            //int amountDigt = 0;
+            //int amountLetter = 0;
+            //int amountPunctuation = 0;
+            //int amountSymbol = 0;
+            //
+            //for(int i = 0; i < sentence.Length;i++)
+            //{
+            //    char c = sentence[i];
+            //
+            //    if (char.IsLetter(c))
+            //    {
+            //        word += c;
+            //        amountLetter++;
+            //    }
+            //    if((c == ' ' && word != string.Empty) || i == sentence.Length -1)
+            //    {
+            //        int index = sentence.IndexOf(word);
+            //        Console.WriteLine($"word: \'{word}\' at {index} length {word.Length}");
+            //        array.Append( word );
+            //        word = string.Empty;
+            //    }
+            //    if(char.IsDigit(c))
+            //    {
+            //        amountDigt++;
+            //    }
+            //    if (char.IsPunctuation(c))
+            //    {
+            //        amountPunctuation++;
+            //    }
+            //    if (char.IsSymbol(c))
+            //    {
+            //        amountSymbol++;
+            //    }
+            //}
+            //
+            //Console.WriteLine("New sentence:");
+            //
+            //foreach (string s in array)
+            //{
+            //    s.Reverse();
+            //    Console.Write($"{s} ");
+            //}
+            //
+            //Console.WriteLine();
+            //Console.WriteLine($"{amountDigt} digit characters");
+            //Console.WriteLine($"{amountLetter} letter characters");
+            //Console.WriteLine($"{amountPunctuation} punctuation characters");
+            //Console.WriteLine($"{amountSymbol} symbol characters");
+            //
+            //Console.ReadLine();
+
+            #endregion
+
+            #region 04
+
+            Console.WriteLine("Guess a number between 0 - 9");
+
+            Random randomGen = new Random();
+
+            int number = randomGen.Next(0,10);
+
+            int guess = 0;
+
+            int tries = 0;
+
+            Console.Write("Your number: ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            tries++;
+
+            while (guess != number)
+            {
+                Console.Write("Your number: ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                tries++;
+            }
+
+            Console.WriteLine($"You found the number {number} after {tries} tries");
+            Console.ReadLine();
+
+            #endregion
+        }
+    }
+}
